7ec28520a94bccda8f98d68af8b69e181bcca18b
eneskarateke <eneskarateke@gmail.com>
2023-07-10 16:17:44 +0300

    final verison

--- api/users/users-router.js
+++ api/users/users-router.js
@@ -3,7 +3,7 @@ const User = require("./users-model");
 
 router.get("/", async (req, res) => {
   try {
-    const users = await User.get();
+    const users = await User.bul();
     res.status(200).json(users);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -13,7 +13,7 @@ router.get("/", async (req, res) => {
 router.get("/:id", async (req, res) => {
   try {
     const id = req.params.id;
-    const user = await User.getById(id);
+    const user = await User.idyeGoreBul(id);
     res.status(200).json(user);
   } catch (error) {
     res.status(500).json({ message: error.message });
@@ -27,7 +27,7 @@ router.post("/", async (req, res) => {
     if (foundedUser) {
       res.status(422).json({ message: "Yazar zaten var" });
     } else {
-      const user = await User.insert({ username: username });
+      const user = await User.ekle({ username: username });
       res.status(201).json(user);
     }
   } catch (error) {
@@ -35,23 +35,23 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.put("/:id", async (req, res) => {
-  try {
-    const { username } = req.body;
-    const user = await User.update({ username: username }, req.params.id);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// router.put("/:id", async (req, res) => {
+//   try {
+//     const { username } = req.body;
+//     const user = await User.update({ username: username }, req.params.id);
+//     res.status(201).json(user);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
-router.delete("/:id", async (req, res) => {
-  try {
-    const user = await User.remove(req.params.id);
-    res.status(201).json(user);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
-});
+// router.delete("/:id", async (req, res) => {
+//   try {
+//     const user = await User.remove(req.params.id);
+//     res.status(201).json(user);
+//   } catch (error) {
+//     res.status(500).json({ message: error.message });
+//   }
+// });
 
 module.exports = router;
